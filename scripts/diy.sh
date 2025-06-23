80b80ffadcd8e0d88fcef0511410b8f22430539d
CC <59593391+CC13594759@users.noreply.github.com>
2025-06-23 21:55:12 +0800

    Add files via upload

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -1,25 +1,3 @@
-# 修改默认IP
-sed -i 's/192.168.1.1/192.168.12.12/g' package/base-files/files/bin/config_generate
-
-# 修改默认主题
-sed -i "s/luci-theme-bootstrap/luci-theme-argon/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
-
-# 修改主机名
-sed -i 's/ImmortalWrt/QWRT/g' package/base-files/files/bin/config_generate
-sed -i 's/ImmortalWrt/QWRT/g' include/version.mk
-sed -i 's/ImmortalWrt/QWRT/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
-
-# 替换 SNAPSHOT 为 (QSDK 12.2)
-sed -i 's/SNAPSHOT/(QSDK 12.2 R7)/g' include/version.mk
-
-
-# 删除luci首页显示
-sed -i '86d' feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
-sed -i "s/+ ' \/ ' : '') + (luciversion ||/:/g" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
-
-# 移除要替换的包
-rm -rf feeds/luci/themes/luci-theme-argon
-
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
   branch="$1" repourl="$2" && shift 2
@@ -30,9 +8,9 @@ function git_sparse_clone() {
   cd .. && rm -rf $repodir
 }
 
-# luci-theme-argon
-git clone --depth=1 https://github.com/sbwml/luci-theme-argon package/luci-theme-argon
-sed -i 's/primary: #5e72e4/primary: #7fdfff/g' package/luci-theme-argon/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
+# passwall2
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
 
 # 修正 Makefile 路径问题
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/luci.mk/$(TOPDIR)\/feeds\/luci\/luci.mk/g' {}
@@ -43,6 +21,35 @@ find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_U
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
+# 修改默认IP
+sed -i 's/192.168.1.1/192.168.12.12/g' package/base-files/files/bin/config_generate
+
+# 修改默认主题
+sed -i "s/luci-theme-bootstrap/luci-theme-argon/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
+sed -i "s/primary:#5e72e4/primary:#7ca7bf/g" $(find ./feeds/luci/themes/luci-theme-argon/ -type f -name "cascade.css")
+
+# 修改主机名
+sed -i 's/ImmortalWrt/QWRT/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/QWRT/g' include/version.mk
+sed -i 's/ImmortalWrt/QWRT/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
+
+# 删除luci首页显示
+sed -i '86d' feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
+sed -i "s/+ ' \/ ' : '') + (luciversion ||/:/g" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
+
+# 替换 SNAPSHOT 为 (QSDK 12.2)
+sed -i 's/SNAPSHOT/(QSDK 12.2 R7)/g' include/version.mk
+sed -i 's/ECM:/ /g' target/linux/qualcommax/base-files/sbin/cpuusage
+sed -i 's/HWE/NPU/g' target/linux/qualcommax/base-files/sbin/cpuusage
+
 # 关闭RFC1918 
 sed -i 's/option rebind_protection 1/option rebind_protection 0/g' package/network/services/dnsmasq/files/dhcp.conf
 
+# 修改插件名字
+sed -i 's/"PassWall 2"/"PassWall"/g' `egrep "PassWall 2" -rl ./`
+
+# 修改插件位置
+sed -i 's/vpn/services/g' feeds/luci/applications/luci-app-zerotier/root/usr/share/luci/menu.d/luci-app-zerotier.json
+
+# etc默认设置
+cp -a $GITHUB_WORKSPACE/scripts/etc/* package/base-files/files/etc/
